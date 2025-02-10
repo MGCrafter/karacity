820b20860f6f96ccfd0012c513e01165c8277ec0
MGCrafter <xjessyx@nekozdevteam.eu>
2025-02-10 01:30:42 +0100

    deletet impressum of footer

--- components/ui_self/footer.js
+++ components/ui_self/footer.js
@@ -3,8 +3,8 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="bg-gray-800 text-white py-4 text-center w-full relative">
-      <p>&copy; {new Date().getFullYear()} Powered by NekoZDevTeam | 
-        <a href="/impressum" className="underline"> Impressum</a>
+      <p>&copy; {new Date().getFullYear()} Powered by NekoZDevTeam 
+        {/*<a href="/impressum" className="underline"> Impressum</a> |*/}
       </p>
     </footer>
   );
